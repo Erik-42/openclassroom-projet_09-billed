00258e6ef597071f870d14faeb16d60e531c7059
Erik-42 <43724032+Erik-42@users.noreply.github.com>
2024-10-22 15:25:28 +0200

    Update NewBill.js
    nettoyage du code final

--- frontend/src/containers/NewBill.js
+++ frontend/src/containers/NewBill.js
@@ -28,12 +28,6 @@ export default class NewBill {
 		const allowedExtensions = ["jpg", "jpeg", "png"];
 		const fileExtension = fileName.split(".").pop();
 
-		// if (!allowedExtensions.includes(fileExtension.toLowerCase())) {
-		// 	alert("Seuls les fichiers JPG, JPEG et PNG sont autorisés.");
-		// 	this.document.querySelector(`input[data-testid="file"]`).value = null; // Réinitialise l'input file
-		// 	return;
-		// }
-
 		const formData = new FormData();
 		const email = JSON.parse(localStorage.getItem("user")).email;
 		formData.append("file", file);
